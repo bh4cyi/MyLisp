7003838e966c297ff51039f733fa6c2d2ac06f59
lqfortytwo <lqfmickey@gmail.com>
2015-11-03 23:42:28 -0500

    Add preprocessor.

--- prompt.c
+++ prompt.c
@@ -1,11 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/* Compiling on Windows */
+#ifdef _WIN32
+#include <string.h>
+
+static char buffer[2048];
+
+/* Fake readline function */
+char* readline(char* prompt)
+{
+    fputs(prompt, stdout);
+    fgets(buffer, 2048, stdin);
+    char* cpy = malloc(strlen(buffer)+1);
+    strcpy(cpy, buffer);
+    cpy[strlen(cpy) - 1] = '\0';
+    return cpy;
+}
+
+/* Fake add_history function */
+void add_history(char* unused)
+{
+}
+
+/* Compiling other OSs */
+#else
 #include <editline/readline.h>
+#endif
 
 /* Declare a buffer for user input of size 2048 */
-static char input[2048];
-
 int main(int argc, char** argv)
 {
 	/* Print Version and Exit Information */
@@ -16,15 +39,11 @@ int main(int argc, char** argv)
     while (1)
     {
         /* Output our prompt */
-        // fputs("Qlisp> ", stdout);
         char* input = readline("QLisp> ");
-        /* Read a line of user input of maxium size 2048 */
-        //fgets(input, 2048, stdin);
         /* Add input to history */
         add_history(input);
         /* Echo input back to user */
         printf("You have just input %s\n", input);
-
         /* Free retrieved input */
         free(input);
      }
